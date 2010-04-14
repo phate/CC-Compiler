ddcde6876f7c6cc8ce5cdb65865965a90d021664
phate <nico.reissmann@gmail.com>
2010-04-14 12:20:20 +0200

    Added jlc.hs

---
+++ jlc.hs
@@ -0,0 +1,51 @@
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.FilePath.Posix (splitExtension)
+import System.Cmd
+import System.IO
+
+import Scanner
+import Parser
+import TypeChecker
+import ErrM
+
+import JVMGenerator
+import JVMPrinter
+import JVMEnv
+
+printEnv :: JVMEnv -> IO ()
+printEnv e = do putStrLn $ "CurrStack: " ++ (show $ currentStackDepth e)
+                putStrLn $ "MaxStack:  " ++ (show $ maxStackDepth e)
+                putStrLn ""
+                return ()
+
+printEnvInfo :: [JVMEnv] -> IO ()
+printEnvInfo envs = mapM_ printEnv envs
+                         
+
+check :: String -> String -> IO ()
+check s file = let tree = parse $ alexScanTokens s in case typecheck tree of
+  Ok (p,env) -> do
+                  hPutStrLn stderr "OK"
+                  let envs = generateInstructions p
+                  let code = getCode file envs
+                  printEnvInfo envs
+                  writeFile (file ++ ".j") code
+                  system $ "java -jar lib/jasmin.jar " ++ file ++ ".j"
+                  return ()
+  Bad err    -> do hPutStrLn stderr "ERROR"
+                   putStrLn err
+                   return()
+
+main :: IO ()
+main = do args <- getArgs
+          case args of
+            [file] -> do
+                        let (file',ext) = splitExtension file
+                        case ext of
+                          ".jl" -> do c <- readFile file
+                                      check c file'
+                          _     -> do putStrLn "No *.jl file as input"
+                                      exitFailure  
+            _      -> do putStrLn "Usage: Compiler <source>"
+                         exitFailure
